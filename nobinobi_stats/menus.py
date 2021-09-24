c937c749dc2bb9928eb94044aef89d698687fed7
Florian Alu <alu@prolibre.com>
2021-09-24 12:32:02 +0200

    Update Copyright

--- nobinobi_stats/menus.py
+++ nobinobi_stats/menus.py
@@ -1,9 +1,9 @@
-#    Copyright (C) Prolibre Sarl 2019 <Florian Alu> <alu@prolibre.com>
+#  Copyright (c) 2021 <Florian Alu - alu@prolibre.com - https://www.prolibre.com>
 #
 #      This program is free software: you can redistribute it and/or modify
 #      it under the terms of the GNU Affero General Public License as
 #      published by the Free Software Foundation, either version 3 of the
-#      License and any later version.
+#      License, or any later version.
 #
 #      This program is distributed in the hope that it will be useful,
 #      but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,6 +12,7 @@
 #
 #      You should have received a copy of the GNU Affero General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 from django.urls import reverse
 from django.utils.translation import gettext as _
